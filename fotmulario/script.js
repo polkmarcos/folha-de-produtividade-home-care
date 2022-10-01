f795627c15a3f8fbf9d18e05a2d3f8d31b33875f
Marcos Carvalho <114582202+polkmarcos@users.noreply.github.com>
2022-10-01 12:46:22 -0300

    aa

---
+++ fotmulario/script.js
@@ -0,0 +1,15 @@
+
+    
+function salvar()
+{
+    var funcionario= document.getElementById(funcionario).innerHTML;
+    if (funcionario.length>=1) {
+       
+    
+      
+
+        }
+        
+}
+
+
